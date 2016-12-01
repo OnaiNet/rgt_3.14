9f0899ee6aa8242d8e70f5f979c9f0f067741871
Steve Keiser <steve@kdata.us>
2016-11-30 21:42:49 -0700

    Actually got it to work.

--- skeiser/Lotto/src/main/java/com/steve/model/Entry.java
+++ skeiser/Lotto/src/main/java/com/steve/model/Entry.java
@@ -1,5 +1,18 @@
 package com.steve.model;
 
 public class Entry {
-
+	private String firstName;
+	private String lastName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
